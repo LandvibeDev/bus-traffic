db7bbc954f9f7da79e52d30a42e51bfdf43242d1
minkyojeong <redwonder@naver.com>
2017-11-12 18:11:07 +0900

    delete hello

--- src/main.js
+++ src/main.js
@@ -1,11 +1,9 @@
 import Vue from "vue"
-import Hello from "./components/Hello.vue"
 import Card from "./components/Card.vue"
 
 let app = new Vue({
     el: '#app',
     components: {
-        hello: Hello,
         card: Card
     }
 });
